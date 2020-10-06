3a2ecaff0005110292e04b22c9220d4bd428b29b
Jack Murphy <jmurphy.web@gmail.com>
2020-10-06 10:33:05 +0200

    #2 - remove new-user save button and persist in on-change event

--- ambue_spike/lib/ambue_spike/accounts/accounts.ex
+++ ambue_spike/lib/ambue_spike/accounts/accounts.ex
@@ -1,9 +1,17 @@
 defmodule AmbueSpike.Accounts do
+  @moduledoc """
+  Accounts context is used to manage users
+  """
+
   alias AmbueSpike.Accounts.User
   alias AmbueSpike.Repo
 
   def new_user_changeset(params \\ %{}) do
     User.changeset(%User{}, params)
+  end
+
+  def user_changeset(user, params) do
+    User.changeset(user, params)
   end
 
   def create_user(params) do
--- ambue_spike/lib/ambue_spike_web/live/user_live.ex
+++ ambue_spike/lib/ambue_spike_web/live/user_live.ex
@@ -9,29 +9,16 @@ defmodule AmbueSpikeWeb.UserLive do
   end
 
   @impl true
-  def handle_event("validate", %{"user" => params}, socket) do
-    changeset =
-      Accounts.new_user_changeset(params)
-      |> Map.put(:action, :insert)
-
-    {:noreply, assign(socket, changeset: changeset)}
-  end
-
-  @impl true
   def handle_event("save", %{"user" => params}, socket) do
     case Accounts.create_user(params) do
       {:ok, user} ->
         {:noreply,
          socket
-         |> assign(user: user)
-         |> put_flash(:info, "user created")}
+         |> put_flash(:info, "saved")
+         |> assign(changeset: Accounts.user_changeset(user, %{}))}
 
       {:error, changeset} ->
-        # ensure error flash is removed
-        {:noreply,
-         socket
-         |> assign(changeset: changeset)
-         |> put_flash(:error, "there was an error")}
+        {:noreply, socket |> assign(changeset: changeset)}
     end
   end
 end
